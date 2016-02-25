fd9b232d63ac86aa2ee87c7a6c3a7c9389422d26
Tom van Dijk <tomvand@hotmail.com>
2016-02-25 14:44:30 +0100

    Add all bluetooth addresses to test layout

--- src/app.js
+++ src/app.js
@@ -8,21 +8,21 @@ var Alm = require('./alm/alm.js');
 var keyboard = require('./sim/keyboardcontroller.js');
 
 var tx_only = [
-    new Beacon(4.07, 0.0, 'Hans'),
+    new Beacon(4.07, 0.0, 'F8:27:73:28:DA:FE'),
     new Beacon(8.20 - 2.01, 0.0, 'C2:92:09:5F:04:78'),
-    new Beacon(0.0, 2.85, 'Muting'),
-    new Beacon(0.0, 5.50, 'Hallway 0'),
+    new Beacon(0.0, 2.85, 'E1:89:95:C1:06:04'),
+    new Beacon(0.0, 5.50, 'F0:20:A1:2C:57:D4'),
     new Beacon(0.0, 12.40 - 2.66, 'C0:82:3E:B9:F5:7B'),
-    new Beacon(8.20, 12.40 - 6.90 + 3.50 + 2.36, 'Hallway 1'),
-    new Beacon(8.20 - 2.449, 12.40 - 6.90 + 0.442, 'Peet')
+    new Beacon(8.20, 12.40 - 6.90 + 3.50 + 2.36, 'E0:31:D7:C5:CA:FF'),
+    new Beacon(8.20 - 2.449, 12.40 - 6.90 + 0.442, 'D7:D5:51:82:49:43')
 ];
 var beacons = [
-    new Beacon(3.05, 4.05, 'ACM0'),
-    new Beacon(3.05, 2.20, 'ACM1'),
-    new Beacon(3.36, 0.00, 'ACM2'),
-    new Beacon(5.25, 0.60, 'ACM0'),
-    new Beacon(5.25, 1.90, 'ACM1'),
-    new Beacon(5.25, 3.95, 'ACM2')
+    new Beacon(3.05, 4.05, 'C4:4C:CA:D7:A6:ED'),
+    new Beacon(3.05, 2.20, 'C6:D6:56:79:24:03'),
+    new Beacon(3.36, 0.00, 'E0:0B:E5:5C:6B:9B'),
+    new Beacon(5.25, 0.60, 'C0:D1:8D:33:4E:29'),
+    new Beacon(5.25, 1.90, 'EB:ED:23:6F:2E:C5'),
+    new Beacon(5.25, 3.95, 'E1:05:D3:D7:D9:81')
 ];
 var all_beacons = tx_only.concat(beacons);
 
