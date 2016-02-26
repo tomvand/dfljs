590ea8d7f01a5a9606d1aebc45e01b203546f17f
Tom van Dijk <tomvand@hotmail.com>
2016-02-26 10:12:46 +0100

    Add RunningVariance
    Added a running variance estimator that will be used to filter out
    background RSSI.

---
+++ src/util/runningvariance.js
@@ -0,0 +1,37 @@
+/**
+ * This module can be used to calculate a rolling average and variance on the
+ * fly.
+ * Based on:
+ * http://stackoverflow.com/questions/5147378/rolling-variance-algorithm
+ * @module
+ */
+
+module.exports = RunningVariance;
+
+function RunningVariance(window) {
+    this._window = window;
+
+    this._psa = 0.0;
+    this._sma = 0.0;
+
+    this.queue = [];
+    for (var i = 0; i < window; i++) {
+        this.queue[i] = 0.0;
+    }
+}
+
+RunningVariance.prototype.filter = function (value) {
+    var old = this.queue.shift();
+    this.queue.push(value);
+
+    this._sma += (value - old) / this._window;
+    this._psa += (value * value - old * old) / this._window;
+};
+
+RunningVariance.prototype.variance = function () {
+    return this._psa - this._sma * this._sma;
+};
+
+RunningVariance.prototype.average = function () {
+    return this._sma;
+};
---
+++ test/util_runningvariance_test.js
@@ -0,0 +1,37 @@
+var assert = require('assert');
+
+var randn = require('../src/util/randn.js');
+
+var RunVar = require('../src/util/runningvariance.js');
+
+describe('RunningVariance', function () {
+    var average = -70.0;
+    var variance = 5.0;
+
+    var numSamples = 100;
+    var samples = [];
+    for (var i = 0; i < numSamples; i++) {
+        samples.push(Math.sqrt(variance) * randn() + average);
+    }
+
+    var runVar = new RunVar(numSamples);
+    samples.forEach(function (sample) {
+        runVar.filter(sample);
+    });
+
+    it('is a function (constructor)', function () {
+        assert.equal(typeof (RunVar), 'function');
+    });
+
+    describe('.average()', function () {
+        it('estimates the average', function () {
+            assert(Math.abs(runVar.average() - average) < 0.5);
+        });
+    });
+
+    describe('.variance()', function () {
+        it('estimates the variance', function () {
+            assert(Math.abs(runVar.variance() - variance) < 0.5);
+        });
+    });
+});
