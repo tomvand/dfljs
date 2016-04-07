94e0d34ad0888ff515ab63da5e9c75a202ea5eb7
Tom van Dijk <tomvand@hotmail.com>
2016-04-07 09:49:56 +0200

    Set Nppt and Naux to 500 as intended

--- src/app/appsim.js
+++ src/app/appsim.js
@@ -14,8 +14,8 @@ var clone = require('clone');
 
 // Set up the tracking filter
 var NMaxTargets = 5;
-var NParticlesPerTarget = 100;
-var NAuxiliaryParticles = 100;
+var NParticlesPerTarget = 500;
+var NAuxiliaryParticles = 500;
 var initInfo = environment.bounds;
 var alm = new AuxPhd(NMaxTargets, NParticlesPerTarget, NAuxiliaryParticles, initInfo, environment.bounds);
 
