a8861455df24fd0065b9e1b49724c2f5e6f50365
Tom van Dijk <tomvand@hotmail.com>
2016-04-28 17:09:44 +0200

    Clean util

--- src/util/runningvariance.js
+++ src/util/runningvariance.js
@@ -3,11 +3,15 @@
  * fly.
  * Based on:
  * http://stackoverflow.com/questions/5147378/rolling-variance-algorithm
- * @module
  */
 
 module.exports = RunningVariance;
 
+/**
+ * Create a running variance filter
+ * @param {number} window - Size of the window
+ * @returns {RunningVariance} Running variance filter
+ */
 function RunningVariance(window) {
     this._window = window;
     this._remainingInitialSamples = window;
@@ -21,6 +25,10 @@ function RunningVariance(window) {
     }
 }
 
+/**
+ * Update the filter
+ * @param {number} value - New sample
+ */
 RunningVariance.prototype.filter = function (value) {
     var old = this.queue.shift();
     this.queue.push(value);
@@ -33,14 +41,27 @@ RunningVariance.prototype.filter = function (value) {
     }
 };
 
+/**
+ * Get the variance over the past window
+ * @returns {Number} variance
+ */
 RunningVariance.prototype.variance = function () {
     return this._psa - this._sma * this._sma;
 };
 
+/**
+ * Get the average over the past window
+ * @returns {Number} average
+ */
 RunningVariance.prototype.average = function () {
     return this._sma;
 };
 
+/**
+ * Is the filter initialized?
+ * @returns {Boolean} True when enough samples have been observed to fill the
+ *      entire window.
+ */
 RunningVariance.prototype.isInitialized = function () {
     return this._remainingInitialSamples === 0;
 };
